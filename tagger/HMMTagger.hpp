a768a3715fef21de29adcbdac905bd504b2b9f54
Joseph Malicki <jmalicki@gmail.com>
2013-07-21 18:24:25 -0400

    1) properly use pseudocounts in HMM 2) fix viterbi evaluation for computational efficiency, iterating over previous state first, then current state.

--- tagger/HMMTagger.hpp
+++ tagger/HMMTagger.hpp
@@ -30,7 +30,12 @@ public:
 public:
 
   double getEmitLogProb(const string& word, int state) const {
-    wordtagprob_t::const_iterator i = wordTagProbs.find(word);
+    string loweredWord(word.size(), '\0');
+
+    std::transform(word.begin(), word.end(),
+		   loweredWord.begin(), (int (*)(int))std::tolower);
+
+    wordtagprob_t::const_iterator i = wordTagProbs.find(loweredWord);
     if (i == wordTagProbs.end())
       i = wordTagProbs.find("");
 
@@ -52,13 +57,22 @@ public:
     // We have marginal tag counts in the word "".
     // We can compute marginal word probabilities by summing word counts.
 
+    // number of pseudo-seen tags (not in training) of each type for each word
+    double tag_pseudocount = 0.5;
+    // number of pseudo-seen words not in training
+    double word_pseudocount = 2;
+
     VectorXi marginalCounts = wordTagCounts[""];
     assert(marginalCounts.size() > 0);
     double total_obs = std::accumulate(&marginalCounts[0],
 				       &marginalCounts[0]+marginalCounts.size(),
 				       0);
+    total_obs += tag_pseudocount * marginalCounts.size() *
+      (wordTagCounts.size()-1+word_pseudocount);
 
-    VectorXd marginalTagProb(marginalCounts.cast<double>().array().log() -
+    VectorXd marginalTagProb((marginalCounts.cast<double>().array()
+			      + tag_pseudocount * (wordTagCounts.size()-1+
+						   word_pseudocount)).log() -
 			     log(total_obs));
 
     for (size_t i = 0; i < marginalTagProb.size(); i++) {
@@ -69,11 +83,13 @@ public:
       double word_obs = std::accumulate(&wordCount.second[0],
 					&wordCount.second[0]+
 					wordCount.second.size(), 0);
+      word_obs += tag_pseudocount * wordCount.second.size();
       
       // P(word) = word_obs / total_obs
       // P(tag|word) = Count(tag|word)/word_obs
       // P(tag|word)*P(word) = Count(tag|word) / total_obs
-      VectorXd thisWordTagProb(wordCount.second.cast<double>().array().log()
+      VectorXd thisWordTagProb((wordCount.second.cast<double>().array()
+				+ tag_pseudocount).log()
 			       - log(total_obs));
       thisWordTagProb -= marginalTagProb;
 
@@ -112,14 +128,14 @@ public:
   virtual ~HMMTagger() {}
 
   virtual void tag(vector<pair<int, string> >& words) const {
-    MatrixXd viterbi(MatrixXd::Constant(tagprobs.cols(), words.size()+1,
+    MatrixXd viterbi(MatrixXd::Constant(tagprobs.cols()-1, words.size()+1,
 					-std::numeric_limits<double>::infinity()
 					));
 
-    MatrixXi backpointer(MatrixXi::Zero(tagprobs.cols(), words.size()));
+    MatrixXi backpointer(MatrixXi::Zero(tagprobs.cols()-1, words.size()));
 
-    for (size_t statenum = 1; statenum < tagprobs.cols(); statenum++) {
-      viterbi(statenum, 0) = log(tagprobs(statenum, 0));
+    for (size_t statenum = 0; statenum < tagprobs.cols()-1; statenum++) {
+      viterbi(statenum, 0) = log(tagprobs(statenum+1, 0));
       assert(viterbi(statenum, 0) <= 0);
       // For ease of iteration, apply emission probabilities only in
       // update step.
@@ -127,22 +143,31 @@ public:
 
     int t = 0;
     for (pair<int, string> word : words) {      
-      for (size_t statenum = 1; statenum < tagprobs.cols(); statenum++) {
-	double max = -std::numeric_limits<double>::infinity();
-	size_t max_backpointer = 0;
-	// We already looked at the initial time above, which is the
-	// only time that can have the initial state, so laststate
-	// will always be something else.
-	for (size_t laststate = 1; laststate < tagprobs.cols() - 1; laststate++) {
-	  // lastState-1 is because initial and final states don't emit.
-	  double thisval = viterbi(laststate, t);
-	  double logtransprob = log(tagprobs(statenum, laststate));
-	  double logemitprob = getEmitLogProb(word.second, laststate-1);
-	  if (thisval > 0) {
-	    std::cerr << "Viterbi logprob > 0 at iteration " << t
-		      << "!  Value " << thisval << std::endl;
-	    throw 0;
-	  }
+      // We already looked at the initial time above, which is the
+      // only time that can have the initial state, so laststate
+      // will always be something else.
+
+      // laststate goes to #cols - 2, since we ignore initial state here,
+      // and final state can't be a previous state, so don't compute it.
+      // We can transition *to* the final state, so we consider that in the
+      // statenum loop
+      for (size_t laststate = 0; laststate < tagprobs.cols() - 2; laststate++) {
+	double prev_viterbi = viterbi(laststate, t);
+	double logemitprob = getEmitLogProb(word.second, laststate);
+	if (prev_viterbi > 0) {
+	  std::cerr << "Viterbi logprob > 0 at iteration " << t
+		    << "!  Value " << prev_viterbi << std::endl;
+	  throw 0;
+	}
+	if (logemitprob > 0) {
+	  std::cerr << "Emission logprob > 0 at iteration " << t
+		    << "!  Value " << logemitprob << std::endl;
+	  throw 0;
+	}
+
+	for (size_t statenum = 0; statenum < tagprobs.cols()-1; statenum++) {
+	  double logtransprob = log(tagprobs(statenum+1, laststate+1));
+	  double thisval = prev_viterbi + logemitprob + logtransprob;
 
 	  if (logtransprob > 0) {
 	    std::cerr << "Transition logprob > 0 at iteration " << t
@@ -150,32 +175,22 @@ public:
 	    throw 0;
 	  }
 
-	  if (logemitprob > 0) {
-	    std::cerr << "Emission logprob > 0 at iteration " << t
-		      << "!  Value " << logemitprob << std::endl;
-	    throw 0;
-	  }
-
-	  thisval += logtransprob + logemitprob;
-
-	  if (thisval > max) {
-	    max = thisval;
-	    max_backpointer = laststate;
+	  if (thisval > viterbi(statenum,t+1)) {
+	    viterbi(statenum, t+1) = thisval;
+	    backpointer(statenum, t) = laststate;
 	  }
 	}
-	backpointer(statenum, t) = max_backpointer;
-	viterbi(statenum, t+1) = max;
       }
       t++;
     }
 
     // Now produce backwords path
-    size_t last_state = tagprobs.cols() - 1;
+    size_t last_state = tagprobs.cols() - 2;
     while (--t >= 0) {
-      words[t].first = backpointer(last_state, t) - 1;
-      last_state = words[t].first + 1;
+      words[t].first = backpointer(last_state, t);
+      last_state = words[t].first;
       assert(words[t].first >= 0);
-      assert(words[t].first < tagprobs.cols()-1);
+      assert(words[t].first < tagprobs.cols()-2);
     }
   }
 
