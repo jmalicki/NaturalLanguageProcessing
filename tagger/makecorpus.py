1ede5be9e400668b178d028cb9c2e1b909d069d3
Joseph Malicki <jmalicki@gmail.com>
2013-07-21 16:54:55 -0400

    make training and test corpuses for simplified and non-simplified tags

--- tagger/makecorpus.py
+++ tagger/makecorpus.py
@@ -2,14 +2,27 @@ from nltk.corpus import brown
 from collections import Counter
 import itertools
 
-with open('/Users/josephmalicki/src/tagger/corpus.tsv', 'w') as outf:
-    for sentence in brown.tagged_sents(simplify_tags=False):
+sentences = brown.tagged_sents(simplify_tags=False)
+with open('testcorpus.tsv', 'w') as outf:
+    for sentence in sentences[1:5000]:
+        for word, tag in sentence:
+            print >>outf, '%s\t%s' % (tag, word)
+        print >>outf, ''
+with open('traincorpus.tsv', 'w') as outf:
+    for sentence in sentences[5000:]:
         for word, tag in sentence:
             print >>outf, '%s\t%s' % (tag, word)
         print >>outf, ''
 
-with open('/Users/josephmalicki/src/tagger/simplified_corpus.tsv', 'w') as outf:
-    for sentence in brown.tagged_sents(simplify_tags=True):
+sentences = brown.tagged_sents(simplify_tags=True)
+with open('simplified_testcorpus.tsv', 'w') as outf:
+    for sentence in sentences[1:5000]:
+        for word, tag in sentence:
+            print >>outf, '%s\t%s' % (tag, word)
+        print >>outf, ''
+
+with open('simplified_traincorpus.tsv', 'w') as outf:
+    for sentence in sentences[5000:]:
         for word, tag in sentence:
             print >>outf, '%s\t%s' % (tag, word)
         print >>outf, ''
